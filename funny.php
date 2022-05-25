457c9fce3c6be6866477c558299d763a12e016fd
qcox <79946044+qcox@users.noreply.github.com>
2022-05-26 00:36:01 +0200

    Update funny.php

--- funny.php
+++ funny.php
@@ -1,4 +1,8 @@
 setfpscap(10)
+repeat game:GetService("RunService").RenderStepped:Wait()
+until game.Players.LocalPlayer.Character:WaitForChild('Humanoid').Health == 100
+    game.Players.LocalPlayer.Character.ForceField:Destroy()
+
 local function findPlayer(name)
 					for _, Player in ipairs(game.Players:GetPlayers()) do
 						if (string.lower(name) == string.sub(string.lower(Player.Name), 1, #name)) then
